cc5f0852545c856c1bf59b82a9434070631fa538
Leonardo Cruz <leonardo.cruztriana@gmail.com>
2018-02-15 19:32:20 -0500

    Se simplifican los test

--- src/test/java/co/bootcamp/exercise/fracciones/FraccionTest.java
+++ src/test/java/co/bootcamp/exercise/fracciones/FraccionTest.java
@@ -9,101 +9,88 @@ public class FraccionTest {
 	
 	@Test(expected= IllegalArgumentException.class)
 	public void creacionFracionesSimplificadas() {
-		new Fraccion.Builder().conDenominador(0)
-									.build();
+		new Fraccion.Builder().conDenominador(0).build();
+	}
+	
+	@Test(expected= IllegalArgumentException.class)
+	public void creacionEnCeroFracionesSimplificadas() {
+		new Fraccion.Builder().conNumerador(0)
+								.conDenominador(0).build();
 	}
 	
 	@Test
 	public void compararFracionesSimplificadas() {
-		Fraccion fraccionDos = new Fraccion.Builder()
+		Fraccion resultadoEsperado = new Fraccion.Builder()
+									.conNumerador(1)
+									.conDenominador(1).build();
+		assertThat(resultadoEsperado, equalTo(new Fraccion.Builder()
 									.conNumerador(4)
-									.conDenominador(4)
-									.build();
-
-		assertThat(new Fraccion.Builder()
-					.conNumerador(1)
-					.conDenominador(1)
-					.build(), equalTo(fraccionDos));		
+									.conDenominador(4).build()));		
 	}
 
 	@Test
 	public void sumarFracionesSimplificadas() {
-		Fraccion fraccion = new Fraccion.Builder()
-									.conNumerador(4)
-									.conDenominador(8)
-									.build();
-
-		assertThat(new Fraccion.Builder()
-				.conNumerador(4)
-				.conDenominador(2)
-				.build(), equalTo(fraccion.sumar(new Fraccion.Builder()
-														.conNumerador(3)
-														.conDenominador(2)
-														.build())));		
+		Fraccion resultadoEsperado = new Fraccion.Builder()
+				.conNumerador(7)
+				.conDenominador(6).build();
+		assertThat(resultadoEsperado, equalTo(new Fraccion.Builder()
+											.conNumerador(2)
+											.conDenominador(3).build()
+											.sumar(new Fraccion.Builder()
+														.conNumerador(1)
+														.conDenominador(2).build())));		
 	}
 	
 	@Test
 	public void sumarFracionesSimplificadas2() {
-		Fraccion fraccion = new Fraccion.Builder()
-									.conNumerador(1)
-									.conDenominador(3)
-									.build();
-
-		assertThat(new Fraccion.Builder()
-				.conNumerador(5)
-				.conDenominador(6)
-				.build(), equalTo(fraccion.sumar(new Fraccion.Builder()
+		Fraccion resultadoEsperado = new Fraccion.Builder()
+									.conNumerador(5)
+									.conDenominador(6).build();
+		assertThat(resultadoEsperado, equalTo(new Fraccion.Builder()
+										.conNumerador(1)
+										.conDenominador(3)
+										.build().sumar(new Fraccion.Builder()
 														.conNumerador(1)
-														.conDenominador(2)
-														.build())));		
+														.conDenominador(2).build())));		
 	}
 	
 	@Test
 	public void restarFracionesSimplificadas() {
-		Fraccion fraccion = new Fraccion.Builder()
+		Fraccion resultadoEsperado = new Fraccion.Builder()
 									.conNumerador(1)
-									.conDenominador(2)
-									.build();
-
-		assertThat(new Fraccion.Builder()
-				.conNumerador(1)
-				.conDenominador(6)
-				.build(), equalTo(fraccion.restar(new Fraccion.Builder()
+									.conDenominador(6).build();
+		assertThat(resultadoEsperado, equalTo(new Fraccion.Builder()
+												.conNumerador(1)
+												.conDenominador(2).build()
+												.restar(new Fraccion.Builder()
 														.conNumerador(1)
-														.conDenominador(3)
-														.build())));		
+														.conDenominador(3).build())));		
 	}
 	
 	@Test
 	public void multiplicarFracionesSimplificadas() {
-		Fraccion fraccion = new Fraccion.Builder()
-									.conNumerador(5)
-									.conDenominador(3)
-									.build();
-
-		assertThat(new Fraccion.Builder()
+		Fraccion resultadoEsperado = new Fraccion.Builder()
 				.conNumerador(5)
-				.conDenominador(6)
-				.build(), equalTo(fraccion.multiplicar(new Fraccion.Builder()
-														.conNumerador(2)
-														.conDenominador(4)
-														.build())));		
+				.conDenominador(6).build();
+		assertThat(resultadoEsperado, equalTo(new Fraccion.Builder()
+						.conNumerador(5)
+						.conDenominador(3).build()
+						.multiplicar(new Fraccion.Builder()
+												.conNumerador(2)
+												.conDenominador(4).build())));		
 	}
 
 	
 	@Test
 	public void dividirFracionesSimplificadas() {
-		Fraccion fraccion = new Fraccion.Builder()
-									.conNumerador(5)
-									.conDenominador(3)
-									.build();
-		
-		assertThat(new Fraccion.Builder()
+		Fraccion resultadoEsperado = new Fraccion.Builder()
 				.conNumerador(10)
-				.conDenominador(3)
-				.build(), equalTo(fraccion.dividir(new Fraccion.Builder()
-														.conNumerador(2)
-														.conDenominador(4)
-														.build())));		
+				.conDenominador(3).build();		
+		assertThat(resultadoEsperado, equalTo(new Fraccion.Builder()
+						.conNumerador(5)
+						.conDenominador(3).build()
+						.dividir(new Fraccion.Builder()
+								.conNumerador(2)
+								.conDenominador(4).build())));		
 	}
 }
